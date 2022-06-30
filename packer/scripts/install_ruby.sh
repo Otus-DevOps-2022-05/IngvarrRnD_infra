2809121e9779e25907c4a7de061bd8f1eddfc061
root <root@bastion.ru-central1.internal>
2022-06-30 20:25:21 +0000

    add packer files

--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo apt update
+sudo rm /var/lib/dpkg/lock-frontend
 sudo apt install -y ruby-full ruby-bundler build-essential
 echo "=====check install ruby and bundler versions===="
 echo $(ruby -v)
