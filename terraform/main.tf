bddb84d44f78eeba5d2352b46d0a46f031cf4ac5
Igor Savilkin <savilkin.igor@gmail.com>
2022-07-03 19:23:27 +0300

    .

---
+++ terraform/main.tf
@@ -0,0 +1,6 @@
+provider "yandex" {
+  token     = "AQAAAABiT4tcAATuwZVqbLbGrkZxnSx_YYK4S2g"
+  cloud_id  = "b1go530u337nds6mirmn"
+  folder_id = "b1g952eluomosq274hif"
+  zone      = "ru-central1-a"
+}
